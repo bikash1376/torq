7dd01cfb4909ffd792d5ba5602dd5fa3aec01a49
bikash1376 <bikash13763@gmail.com>
2026-02-06 18:35:50 +0530

    tambo setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { MessageThreadCollapsible } from "@/src/components/tambo/message-thread-collapsible";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <MessageThreadCollapsible />
+    </main>
+  );
+}
